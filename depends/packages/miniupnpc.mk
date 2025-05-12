1f65db9328c67f68efe64bf7ce7cc8a1e3ffc03e
AlefLM <aleflm@proton.me>
2025-05-12 18:49:44 +0330

    Update package miniupnpc

--- depends/packages/miniupnpc.mk
+++ depends/packages/miniupnpc.mk
@@ -1,8 +1,8 @@
 package=miniupnpc
-$(package)_version=2.0.20170509
+$(package)_version=2.1.20201016
 $(package)_download_path=http://miniupnp.free.fr/files
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=d3c368627f5cdfb66d3ebd64ca39ba54d6ff14a61966dbecb8dd296b7039f16a
+$(package)_sha256_hash=69f72fe355f911b807915f78dcfc0af772c0f22bc7ab1157e70f334e37db7d39
 
 define $(package)_set_vars
 $(package)_build_opts=CC="$($(package)_cc)"
