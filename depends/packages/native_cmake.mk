4d33b04357215dee126318807ae87119c2cd72f0
AlefLM <aleflm@proton.me>
2025-05-12 19:58:34 +0330

    Update package native_cmake

--- depends/packages/native_cmake.mk
+++ depends/packages/native_cmake.mk
@@ -1,8 +1,8 @@
 package=native_cmake
-$(package)_version=3.14.7
-$(package)_download_path=https://cmake.org/files/v3.14/
+$(package)_version=4.0.2
+$(package)_download_path=https://cmake.org/files/v4.0/
 $(package)_file_name=cmake-$($(package)_version).tar.gz
-$(package)_sha256_hash=9221993e0af3e6d10124d840ff24f5b2f3b884416fca04d3312cb0388dec1385
+$(package)_sha256_hash=1c3a82c8ca7cf12e0b17178f9d0c32f7ac773bd5651a98fcfd80fbf4977f8d48
 
 define $(package)_set_vars
   $(package)_config_opts=
