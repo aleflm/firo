fbf31108f6efcc9ddc2524f38201846e8cd6d495
AlefLM <aleflm@proton.me>
2025-05-12 20:01:32 +0330

    Update package native_ds_store

--- depends/packages/native_ds_store.mk
+++ depends/packages/native_ds_store.mk
@@ -1,8 +1,8 @@
 package=native_ds_store
-$(package)_version=1.3.0
+$(package)_version=1.3.1
 $(package)_download_path=https://github.com/al45tair/ds_store/archive/
 $(package)_file_name=v$($(package)_version).tar.gz
-$(package)_sha256_hash=76b3280cd4e19e5179defa23fb594a9dd32643b0c80d774bd3108361d94fb46d
+$(package)_sha256_hash=4b09f5eb6ba13c4f2350eb8761726e88e30aa329bf790de7ae513e465a681e57
 $(package)_install_libdir=$(build_prefix)/lib/python3/dist-packages
 
 define $(package)_build_cmds
